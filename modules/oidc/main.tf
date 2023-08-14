39766a8b495803ad2bd6d30cd0ad89931f6e0037
null <platform-engineering@redhat.com>
2023-08-14 00:34:16 +0000

    chore(deps): update all dependencies

--- modules/oidc/main.tf
+++ modules/oidc/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.11.0"
+      version = "5.12.0"
     }
     http = {
       source  = "hashicorp/http"
@@ -14,7 +14,7 @@ terraform {
     }
     github = {
       source  = "integrations/github"
-      version = "5.32.0"
+      version = "5.33.0"
     }
   }
 }
